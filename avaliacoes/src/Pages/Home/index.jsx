dded2742e5037a465b8492268b7353c461e0a810
Balbino-teste <balbinojteste@gmail.com>
2025-05-09 21:35:47 -0300

    Alteração do projeto
    Alteração do projeto e finalização com integração com o banco de dados mongoDB.

--- avaliacoes/src/Pages/Home/index.jsx
+++ avaliacoes/src/Pages/Home/index.jsx
@@ -1,42 +1,72 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import api from '../../services/api'
 import './style.css'
 
 function Home() {
- 
-  let users = []
+  const [users, setUsers] = useState([])
+  const [nome, setNome] = useState('')
+  const [depoimento, setDepoimento] = useState('')
 
-  async function getUsers(){
-    const usersFromApi = await api.get('/usuarios')
+  async function getUsers() {
+    try {
+      const response = await api.get('/usuarios')
+      setUsers(response.data)
+    } catch (error) {
+      console.error('Erro ao buscar usuários', error)
+    }
+  }
 
-    users = usersFromApi.data
-    console.log(users)
+  async function handleSubmit(e) {
+    e.preventDefault()
+    if (!nome || !depoimento) return alert('Preencha todos os campos!')
+
+    try {
+      await api.post('/usuarios', {
+        name: nome,
+        testimony: depoimento
+      })
+      setNome('')
+      setDepoimento('')
+      getUsers() // Atualiza a lista com o novo usuário
+    } catch (error) {
+      console.error('Erro ao enviar depoimento', error)
+    }
   }
 
   useEffect(() => {
     getUsers()
-  },[])
+  }, [])
 
-
-   return (
+  return (
     <div className='container'>
-      <form>
-        <h1>Avaliações de nosso clientes</h1>
-        <input name='nome' type='text'placeholder='Digite seu nome'/>
-        <textarea name="depoimento" id="testemonials" placeholder='Deixe sua mensagem'></textarea>
-        <button type='button'>Enviar</button>
+      <form onSubmit={handleSubmit}>
+        <h1>Avaliações de nossos clientes</h1>
+        <input
+          name='nome'
+          type='text'
+          placeholder='Digite seu nome'
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <textarea
+          name='depoimento'
+          id='testemonials'
+          placeholder='Deixe sua mensagem'
+          value={depoimento}
+          onChange={(e) => setDepoimento(e.target.value)}
+        ></textarea>
+        <button type='submit'>Enviar</button>
       </form>
 
       {users.map((user) => (
-           <div key={user.id} className='card'>
-              <div>
-                <p>Nome: <span>{user.name}</span></p>
-                <p>Depoimento: <span>{user.depoimento}</span></p>
-              </div>
-              <button><i className="fa-solid fa-trash"></i></button>
-            </div>
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Depoimento: <span>{user.testimony}</span></p> {/* Corrigido aqui */}
+          </div>
+          <button><i className="fa-solid fa-trash"></i></button>
+        </div>
       ))}
-     
     </div>
   )
 }
--- avaliacoes/src/services/api.js
+++ avaliacoes/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
-
 const api = axios.create({
-    baseURL: 'http://lacalhost:3000'
+  baseURL: 'http://localhost:3000' 
 })
 
 export default api
